68111c57e288eac65861b8a0fbdc0c5dd2a59693
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-09-30 21:03:30 +0900

    feat gsapsample

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,51 @@
 //scss
 import '../scss/style.scss'
 
-//Splide
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css';
-import '@splidejs/splide/css/skyblue';
-import '@splidejs/splide/css/sea-green';
-import '@splidejs/splide/css/core';
+//gsap 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Flip from "gsap/Flip";
+import Draggable from "gsap/Draggable";
 
-new Splide('.splide').mount();
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('.logo');
+const menu = document.querySelector('.menu');
+const headline = document.querySelector('.headline');
+
+const tl = gsap.timeline();
+
+tl.fromTo(hero, 1, {height:'0%'},{ height:'80%'})
+.fromTo(
+    hero,1.2,
+    {width:'100%'},
+    {width:"80%"}
+)
+.fromTo(
+    slider,
+    1.2,
+    {y:'-100%'},
+    {y:"0%"},
+    '-=1.2'
+)
+.fromTo(
+    logo,
+    0.5,
+    {opacity:0,x:30},
+    {opacity:1,x:0},
+    "-=0.5"
+)
+.fromTo(
+    menu,
+    0.5,
+    {opacity:0,x:30},
+    {opacity:1,x:0},
+    "-=0.5"
+)
+.fromTo(
+    headline,
+    0.5,
+    {opacity:0,x:0},
+    {opacity:1,x:30},
+    "-=0.5"
+);
