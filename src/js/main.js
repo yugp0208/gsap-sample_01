993af84f71cbf5324a95887f8fb5a26e69bfc4cb
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-10-01 14:51:07 +0900

    clean コメント記述

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,22 @@
 //scss
 import '../scss/style.scss'
 
-//gsap 
+//gsapの読み込み
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Flip from "gsap/Flip";
 import Draggable from "gsap/Draggable";
 
+//各要素取得
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
 const logo = document.querySelector('.logo');
 const menu = document.querySelector('.menu');
 const headline = document.querySelector('.headline');
 
-const tl = gsap.timeline();
+const tl = gsap.timeline();//初期化
 
+//timelineのアニメーション追加
 tl.fromTo(hero, 1, {height:'0%'},{ height:'80%'})
 .fromTo(
     hero,1.2,
